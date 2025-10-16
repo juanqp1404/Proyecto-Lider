e7b3a3e9eb8344e9d2ddf61e6435373bd2a4d579
Juanqp1404 <juanqp1404@gmail.com>
2025-10-15 20:04:23 -0500

    ariba exportar archivo

--- ariba.py
+++ ariba.py
@@ -1,16 +1,48 @@
+import os
 import re
 import time
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+# TODO: cerrar proceso msedge si esta en segundo plano con task manager antes de ejecutar
 
 def run(playwright: Playwright) -> None:
     user_data_dir = r"C:/Users/JQuintero27/AppData/Local/Microsoft/Edge/User Data"
-
+    carpeta_actual = "./"
     # browser = playwright.chromium.launch(headless=False,executable_path='C:/Program Files/Google/Chrome/Application/chrome.exe')
     context = playwright.chromium.launch_persistent_context(user_data_dir, headless=False, channel="msedge")
     page = context.new_page()
     page.goto("https://s1.ariba.com/Sourcing/Main/aw?awh=r&awssk=ODTzCxAIbpbHwlV1&realm=schlumberger")
-    time.sleep(40)
+    # time.sleep(40)
+    page.get_by_role("button", name="Manage").click()
+    time.sleep(2)
+    page.get_by_role("menuitem", name="Queues").click()
+    time.sleep(2)
+    page.locator("#text__c_npbb").click()
+    time.sleep(2)
+    page.get_by_role("option", name="Requisition").click()
+    time.sleep(2)
+    page.get_by_role("textbox", name="From:").fill("Sun, 7 Sep, 2025")
+    time.sleep(2)
+    page.get_by_title("Run this search").click()
+    time.sleep(2)
+    # page.get_by_role("button", id="_7msd8").click()
+    page.query_selector('#_7msd8')
+    time.sleep(2)
+
+    with page.expect_download() as download_info:
+        page.get_by_role("menuitem", name="Export all Rows").click()
+
+    download = download_info.value
+
+    new_filename = "DF"
+    print(f"Nombre sugerido del archivo: {download.suggested_filename}")
+    print(f"Descargando el archivo como: {new_filename}")
+    print(f"URL del archivo: {download}")
+    time.sleep(20)
+    # Guardar el archivo en la carpeta actual con su nombre original
+    ruta_destino = os.path.join(carpeta_actual, new_filename)
+    download.save_as(ruta_destino)
+
     # page.goto("https://example.com/")
     # page.get_by_role("heading", name="Example Domain").click()
     # page.get_by_role("link", name="More information...").click()
@@ -24,7 +56,7 @@ def run(playwright: Playwright) -> None:
     # time.sleep(305)
 
     context.close()
-    browser.close()
+    # browser.close()
 
 
 with sync_playwright() as playwright:
